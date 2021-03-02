59910f892594da002e439343d3c19a4905057c87
yamap55 <tetuya120@hotmail.com>
2021-03-02 22:56:10 +0900

    [add] abc193

---
+++ contests/20210227/abc193/c/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+from bisect import bisect, bisect_left, bisect_right, insort, insort_left, insort_right  # type: ignore
+from collections import Counter, defaultdict, deque  # type: ignore
+from fractions import gcd  # type: ignore
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace, merge  # type: ignore
+from itertools import accumulate, combinations, permutations, product  # type: ignore
+import math
+
+
+LI = lambda: list(map(int, sys.stdin.buffer.readline().split()))
+I = lambda: int(sys.stdin.buffer.readline())
+LS = lambda: sys.stdin.buffer.readline().rstrip().decode("utf-8").split()
+S = lambda: sys.stdin.buffer.readline().rstrip().decode("utf-8")
+IR = lambda n: [I() for _ in range(n)]
+LIR = lambda n: [LI() for _ in range(n)]
+SR = lambda n: [S() for _ in range(n)]
+LSR = lambda n: [LS() for _ in range(n)]
+SRL = lambda n: [list(S()) for _ in range(n)]
+MSRL = lambda n: [[int(i) for i in list(S())] for _ in range(n)]
+
+n = I()
+
+a = int(math.sqrt(n))
+
+c = 0
+s = set()
+for i in range(2, a + 1):
+    x = 2
+    while True:
+        z = i ** x
+        if z <= n:
+            s.add(z)
+            # print(z)
+        else:
+            break
+        x += 1
+print(n - len(s))
